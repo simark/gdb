f88a4ea6dd12df96114e5089376720f8068ff55c
Pedro Alves <palves@redhat.com>
2015-02-24 22:52:08 +0000

    record-full/record-btrace: software/hardware breakpoint trap
    This adjusts the record targets to tell the core whether a trap was
    caused by a breakpoint.  Targets that can do this should report
    breakpoint traps with the PC already adjusted, so this removes the
    re-incrementing record-full was doing.
    
    These targets need to be adjusted before targets beneath are,
    otherwise target_supports_stopped_by_sw_breakpoint, etc. would fall
    through to the target beneath, and the core would get confused.
    
    Tested on x86-64 Fedora 20, native and gdbserver.
    
    gdb/ChangeLog:
    2015-02-25  Pedro Alves  <palves@redhat.com>
    
    	* btrace.h: Include target/waitstatus.h.
    	(struct btrace_thread_info) <stop_reason>: New field.
    	* record-btrace.c (record_btrace_step_thread): Use
    	record_check_stopped_by_breakpoint instead of breakpoint_here_p.
    	(record_btrace_decr_pc_after_break): Delete.
    	(record_btrace_stopped_by_sw_breakpoint)
    	(record_btrace_supports_stopped_by_sw_breakpoint)
    	(record_btrace_stopped_by_hw_breakpoint)
    	(record_btrace_supports_stopped_by_hw_breakpoint): New functions.
    	(init_record_btrace_ops): Install them.
    	* record-full.c (record_full_hw_watchpoint): Delete and replace
    	with ...
    	(record_full_stop_reason): ... this throughout.
    	(record_full_exec_insn): Adjust.
    	(record_full_wait_1): Adjust.  No longer re-increment the PC.
    	(record_full_wait_1): Adjust.  Use
    	record_check_stopped_by_breakpoint instead of breakpoint_here_p.
    	(record_full_stopped_by_watchpoint): Adjust.
    	(record_full_stopped_by_sw_breakpoint)
    	(record_full_supports_stopped_by_sw_breakpoint)
    	(record_full_supports_stopped_by_sw_breakpoint)
    	(record_full_stopped_by_hw_breakpoint)
    	(record_full_supports_stopped_by_hw_breakpoint): New functions.
    	(init_record_full_ops, init_record_full_core_ops): Install them.
    	* record.c (record_check_stopped_by_breakpoint): New function.
    	* record.h: Include target/waitstatus.h.
    	(record_check_stopped_by_breakpoint): New declaration.

--- gdb/btrace.h
+++ gdb/btrace.h
@@ -27,6 +27,7 @@
    list of sequential control-flow blocks, one such list per thread.  */
 
 #include "btrace-common.h"
+#include "target/waitstatus.h" /* For enum target_stop_reason.  */
 
 struct thread_info;
 struct btrace_function;
@@ -261,6 +262,9 @@ struct btrace_thread_info
      Gaps are skipped during replay, so REPLAY always points to a valid
      instruction.  */
   struct btrace_insn_iterator *replay;
+
+  /* Why the thread stopped, if we need to track it.  */
+  enum target_stop_reason stop_reason;
 };
 
 /* Enable branch tracing for a thread.  */
